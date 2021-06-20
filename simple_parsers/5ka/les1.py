e56582ef4af6e627a84129e2f3aacfa5d19efc81
Radif <i@radif.ru>
2021-06-20 16:49:05 +0300

    all parsers

---
+++ simple_parsers/5ka/les1.py
@@ -0,0 +1,19 @@
+"""
+HTTP/s
+Headers:
+Body:
+
+Методы запросов
+GET
+POST
+PUT
+PATCH
+DELETE
+INFO
+
+1xx, - Information
+2xx, - OK
+3xx, - Redirect
+4xx, - Client Error
+5xx, - Server Error
+"""
---
+++ simple_parsers/5ka_other/les1.py
@@ -0,0 +1,17 @@
+"""
+HTTP/s
+Headers:
+Body:
+Методы запросов
+GET
+POST
+PUT
+PATCH
+DELETE
+INFO
+1xx, - Information
+2xx, - OK
+3xx, - Redirect
+4xx, - Client Error
+5xx, - Server Error
+"""
